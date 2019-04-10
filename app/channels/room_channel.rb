a3efa46d8aa892f3f7b38d55ad7e8b1c83d04919
tanakanaoko <nakochi.xxx@gmail.com>
2019-04-10 22:11:55 +0900

    real time chat

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -7,7 +7,7 @@ class RoomChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
-    ActionCable.server.broadcast 'room_channel', message: data['message']
+  def speak(data)
+    Message.create! content: data['message']
   end
 end
