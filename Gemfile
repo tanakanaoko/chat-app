e111141c36d0d9755fc6b9bdc0d2a71f840f28ef
tanakanaoko <nakochi.xxx@gmail.com>
2019-04-27 22:44:58 +0900

    routeがおわってる

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.3.3'
 
 gem 'devise'
+gem 'omniauth-facebook'
+gem 'omniauth-twitter'
+gem 'omniauth-google-oauth2'
 gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_133414) do
+ActiveRecord::Schema.define(version: 2019_04_27_131508) do
 
-  create_table "messages", force: :cascade do |t|
-    t.text "content"
+# Could not dump table "messages" because of following StandardError
+#   Unknown type 'referencese' for column 'room'
+
+  create_table "rooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -26,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_04_09_133414) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
